9ddb4debfe1614b7cd5234d43e72e7d608c8e8ff
markperrycr02 <mark_perry_cr02@yahoo.com>
2025-09-18 13:52:25 -0300

    changed some code

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -32,7 +32,7 @@ export default async function DashboardRoute() {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data.map((item) => (
-          <BlogpostCard data={item} key={item.id} />
+          <BlogpostCard data={item} key={item?.id} />
         ))}
       </div>
     </div>
