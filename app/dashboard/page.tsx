2613188d88425b229f898dd6b8c95226e40c0b4a
markperrycr02 <mark_perry_cr02@yahoo.com>
2025-09-18 12:48:55 -0300

    added || ""

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,7 +21,7 @@ export async function getData(userId: string) {
 export default async function DashboardRoute() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  const data = await getData(user!.id);
+  const data = await getData(user?.id || "");
   return (
     <div className="py-8">
       <div className="flex items-center justify-between mb-4">
