f7f257da6f5348321861b9dcfca3eee8c2d40c40
markperrycr02 <mark_perry_cr02@yahoo.com>
2025-09-19 13:17:39 -0300

    added useKindBrowserClient to NavBar component

--- components/general/NavBar.tsx
+++ components/general/NavBar.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
-export default async function NavBar() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+export default function NavBar() {
+  const { getUser } = useKindeBrowserClient();
+  const user = getUser();
 
   return (
     <nav className="py-5 flex items-center justify-between">
