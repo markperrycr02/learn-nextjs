0e49ad6bc0207b76e7d1c615f729f385afcd1943
markperrycr02 <mark_perry_cr02@yahoo.com>
2025-09-23 20:43:47 -0300

    changed button to a link

--- components/general/DeletePostButton.tsx
+++ components/general/DeletePostButton.tsx
@@ -1,15 +1,16 @@
 "use client";
+import Link from "next/link";
 // import { deleteSinglePost } from "@/app/actions";
 import { Button } from "../ui/button";
 
 export default function DeletePostButton() {
   return (
-    <Button
+    <Link
       // onClick={() => deleteSinglePost(postId)}
-      className="cursor-pointer text-rounded-sm text-white"
-      variant="destructive"
+      className="cursor-pointer text-rounded-sm text-red-600 hover:text-red-800"
+      href="#"
     >
       delete post
-    </Button>
+    </Link>
   );
 }
